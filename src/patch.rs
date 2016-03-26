9b93e68ec12fbbc00ad945ea867e622ddd4b666f
Nathan Sobo <nathan@github.com>
2016-03-26 08:15:04 -0600

    WIP: Add to Point implementation; lifetime problem

--- src/patch.rs
+++ src/patch.rs
@@ -1,21 +1,81 @@
+use std::rc::Rc;
+use std::rc::Weak;
 use point::Point;
 
-pub struct Patch {
-    root: Option<Box<Node>>
+pub struct Patch<'a> {
+    root: Option<Rc<Node>>,
+    cursor: Option<Cursor<'a>>
 }
 
-impl Patch {
-    pub fn new() -> Patch {
-        Patch {root: None}
+impl<'a> Patch<'a> {
+    pub fn new() -> Patch<'a> {
+        let mut patch = Patch {root: None, cursor: None};
+        patch.cursor = Some(Cursor::new(&mut patch.root));
+        patch
     }
 
-    pub fn splice_with_text(&self, start: &Point, old_extent: &Point, new_text: &str) {
+    pub fn splice_with_text(&self, start: &Point, old_text: &str, new_text: &str) {
+        let old_extent = Point::extent(old_text);
         let new_extent = Point::extent(new_text);
+        self.splice(start, &old_extent, &new_extent, old_text, new_text);
+    }
+
+    pub fn splice(&self, start: &Point, old_extent: &Point, new_extent: &Point, old_text: &str, new_text: &str) {
+        if old_extent.is_zero() && new_extent.is_zero() { return; }
+
+        let output_old_end = start.traverse(old_extent);
+        let output_new_end = start.traverse(new_extent);
+
+        // let start_node = self.cursor.insert_splice_boundary(start);
     }
 }
 
 pub struct Node {
-    parent: Option<Box<Node>>,
-    left_child: Option<Box<Node>>,
-    right_child: Option<Box<Node>>
+    parent: Option<Weak<Node>>,
+    left_child: Option<Rc<Node>>,
+    right_child: Option<Rc<Node>>
+}
+
+struct Cursor<'a> {
+    current_node: &'a mut Option<Rc<Node>>,
+    input_start: Point,
+    output_start: Point,
+
+    left_ancestor: Option<&'a Node>,
+    left_ancestor_input_position: Option<Point>,
+    left_ancestor_output_position: Option<Point>,
+    left_ancestor_stack: Vec<&'a Node>,
+    left_ancestor_input_position_stack: Vec<Point>,
+    left_ancestor_output_position_stack: Vec<Point>,
+
+    right_ancestor: Option<&'a Node>,
+    right_ancestor_input_position: Option<Point>,
+    right_ancestor_output_position: Option<Point>,
+    right_ancestor_stack: Vec<&'a Node>,
+    right_ancestor_input_position_stack: Vec<Point>,
+    right_ancestor_output_position_stack: Vec<Point>
+}
+
+impl<'a> Cursor<'a> {
+    fn new(current_node: &'a mut Option<Rc<Node>>) -> Cursor {
+        Cursor {
+            current_node: current_node,
+            input_start: Point::zero(),
+            output_start: Point::zero(),
+
+            left_ancestor: None,
+            left_ancestor_input_position: None,
+            left_ancestor_output_position: None,
+            left_ancestor_stack: vec![],
+            left_ancestor_input_position_stack: vec![],
+            left_ancestor_output_position_stack: vec![],
+
+            right_ancestor: None,
+            right_ancestor_input_position: None,
+            right_ancestor_output_position: None,
+            right_ancestor_stack: vec![],
+            right_ancestor_input_position_stack: vec![],
+            right_ancestor_output_position_stack: vec![]
+        }
+    }
 }
--- src/point.rs
+++ src/point.rs
@@ -1,3 +1,6 @@
+use std::cmp::Ordering;
+
+#[derive(Eq, PartialEq, Debug)]
 pub struct Point {
     pub row: i32,
     pub column: i32
@@ -6,6 +9,10 @@ pub struct Point {
 impl Point {
     pub fn new(row: i32, column: i32) -> Point {
         Point {row: row, column: column}
+    }
+
+    pub fn zero() -> Point {
+        Point {row: 0, column: 0}
     }
 
     pub fn extent(text: &str) -> Point {
@@ -17,5 +24,55 @@ impl Point {
         }
 
         Point::new(row, column)
+    }
+
+    pub fn is_zero(&self) -> bool {
+        self.row == 0 && self.column == 0
+    }
+
+    pub fn traverse(&self, distance: &Point) -> Point {
+        if distance.row == 0 {
+            Point::new(self.row, self.column + distance.column)
+        } else {
+            Point::new(self.row + distance.row, distance.column)
+        }
+    }
+}
+
+impl Ord for Point {
+    fn cmp(&self, other: &Point) -> Ordering {
+        match self.row.cmp(&other.row) {
+            Ordering::Less => Ordering::Less,
+            Ordering::Equal => self.column.cmp(&other.column),
+            Ordering::Greater => Ordering::Greater
+        }
+    }
+}
+
+impl PartialOrd for Point {
+    fn partial_cmp(&self, other: &Point) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Point;
+
+    #[test]
+    fn ordering() {
+        assert!(Point::new(0, 0) == Point::new(0, 0));
+        assert!(Point::new(0, 0) < Point::new(1, 0));
+        assert!(Point::new(0, 0) < Point::new(0, 1));
+        assert!(Point::new(1, 0) > Point::new(0, 0));
+        assert!(Point::new(0, 1) > Point::new(0, 0));
+    }
+
+    #[test]
+    fn traversal() {
+        assert_eq!(Point::new(0, 5).traverse(&Point::new(0, 10)), Point::new(0, 15));
+        assert_eq!(Point::new(0, 10).traverse(&Point::new(0, -5)), Point::new(0, 5));
+        assert_eq!(Point::new(0, 5).traverse(&Point::new(2, 10)), Point::new(2, 10));
+        assert_eq!(Point::new(2, 10).traverse(&Point::new(-2, 5)), Point::new(0, 5));
     }
 }
