b701856012c35c146e740f8a895ccdc5776f8e88
Nathan Sobo <nathan@github.com>
2016-03-28 00:35:42 -0600

    OMG it compiles. Still incomplete. Hell of a learning curve.

--- src/patch.rs
+++ src/patch.rs
@@ -23,87 +23,78 @@ impl<'a> Patch<'a> {
         let old_end = start.traverse(old_extent);
         let new_end = start.traverse(new_extent);
 
-        let lower_bound = self.find_lower_bound(start).map(|node| self.splay_node(node));
-        let upper_bound = self.find_upper_bound(&old_end).map(|node| self.splay_node(node));
+        let lower_bound = self.find_lower_bound(start);
+        lower_bound.map(|node| self.splay_node(node.clone()));
 
+        let upper_bound = self.find_upper_bound(&old_end);
+        upper_bound.map(|node| self.splay_node(node.clone()));
     }
 
-    fn find_lower_bound(&self, target: &Point) -> Option<Rc<Node>> {
-        self.root.and_then(|root| {
-            let mut left_ancestor_end = Point::zero();
+    fn find_lower_bound(&self, target: &Point) -> Option<Rc<Node<'a>>> {
+        self.root.as_ref().and_then(|root| {
+            let mut current_node: Rc<Node> = root.clone();
             let mut max_lower_bound: Option<Rc<Node>> = None;
-            let mut current_node = root;
+            let mut left_ancestor_end = Point::zero();
 
             loop {
                 let current_node_start = left_ancestor_end.traverse(&current_node.new_distance_from_left_ancestor);
                 if target < &current_node_start {
-                    match current_node.left_child {
-                        Some(left_child) => {
-                            current_node = left_child;
-                        },
-                        None => {
-                            return max_lower_bound;
-                        }
+                    if let None = current_node.left_child {
+                        return max_lower_bound;
+                    } else {
+                        current_node = current_node.left_child.as_ref().unwrap().clone();
                     }
                 } else {
-                    max_lower_bound = Some(current_node);
-                    match current_node.right_child {
-                        Some(right_child) => {
-                            current_node = right_child;
-                            left_ancestor_end = current_node_start.traverse(&current_node.new_extent);
-                        },
-                        None => {
-                            return max_lower_bound;
-                        }
+                    max_lower_bound = Some(current_node.clone());
+
+                    if let None = current_node.right_child {
+                        return max_lower_bound;
+                    } else {
+                        left_ancestor_end = current_node_start.traverse(&current_node.new_extent);
+                        current_node = current_node.right_child.as_ref().unwrap().clone();
                     }
                 }
             }
         })
     }
 
-    fn find_upper_bound(&self, target: &Point) -> Option<Rc<Node>> {
-        self.root.and_then(|root| {
-            let mut left_ancestor_end = Point::zero();
+    fn find_upper_bound(&self, target: &Point) -> Option<Rc<Node<'a>>> {
+        self.root.as_ref().and_then(|root| {
+            let mut current_node: Rc<Node> = root.clone();
             let mut min_upper_bound: Option<Rc<Node>> = None;
-            let mut current_node = root;
+            let mut left_ancestor_end = Point::zero();
 
             loop {
                 let current_node_end = left_ancestor_end
                     .traverse(&current_node.new_distance_from_left_ancestor)
                     .traverse(&current_node.new_extent);
-                if target < &current_node_end {
-                    min_upper_bound = Some(current_node);
-                    match current_node.left_child {
-                        Some(left_child) => {
-                            current_node = left_child;
-                        },
-                        None => {
-                            return min_upper_bound;
-                        }
+                if target <= &current_node_end {
+                    min_upper_bound = Some(current_node.clone());
+
+                    if let None = current_node.left_child {
+                        return min_upper_bound;
+                    } else {
+                        current_node = current_node.left_child.as_ref().unwrap().clone();
                     }
                 } else {
-                    match current_node.right_child {
-                        Some(right_child) => {
-                            current_node = right_child;
-                            left_ancestor_end = current_node_end
-                        },
-                        None => {
-                            return min_upper_bound;
-                        }
+                    if let None = current_node.right_child {
+                        return min_upper_bound;
+                    } else {
+                        left_ancestor_end = current_node_end.traverse(&current_node.new_extent);
+                        current_node = current_node.right_child.as_ref().unwrap().clone();
                     }
                 }
             }
         })
     }
 
-    fn splay_node(&'a mut self, node: Rc<Node>) -> Rc<Node> {
-
+    fn splay_node(&mut self, node: Rc<Node>) {
     }
 
-    fn rotate_node_right(&'a mut self, pivot: Rc<Node>) {
+    fn rotate_node_right(&mut self, pivot: Rc<Node>) {
     }
 
-    fn rotate_node_left(&'a mut self, pivot: Rc<Node<'a>>) {
+    fn rotate_node_left(&mut self, pivot: Rc<Node>) {
     }
 }
 
@@ -128,26 +119,26 @@ impl<'a> PartialEq for Node<'a> {
 
 impl<'a> Node<'a> {
     pub fn is_left_child(&self) -> bool {
-        self.get_parent()
+        self.get_parent().as_ref()
             .and_then(|parent| {
-                parent.left_child.map(|parent_left_child| {
-                    self == &*parent_left_child
+                parent.left_child.as_ref().map(|left_sibling| {
+                    self == &**left_sibling
                 })
             })
             .unwrap_or(false)
     }
 
     pub fn is_right_child(&self) -> bool {
-        self.get_parent()
+        self.get_parent().as_ref()
             .and_then(|parent| {
-                parent.right_child.map(|parent_right_child| {
-                    self == &*parent_right_child
+                parent.right_child.as_ref().map(|right_sibling| {
+                    self == &**right_sibling
                 })
             })
             .unwrap_or(false)
     }
 
     pub fn get_parent(&self) -> Option<Rc<Node<'a>>> {
-        self.parent.and_then(|parent| { parent.upgrade() })
+        self.parent.as_ref().and_then(|parent| { parent.upgrade() })
     }
 }
